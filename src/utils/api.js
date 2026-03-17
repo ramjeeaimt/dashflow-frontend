3f2fa1118c5c02c2f2343c29485ae9eaa36f0cb3
Ramjee194 <ramjeekumaryadav733@gmail.com>
2026-03-17 19:05:33 +0530

    changes in payroll

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-     baseURL: 'http://localhost:3000',
-   // baseURL: 'https://difmo-crm-backend.vercel.app',
+    // baseURL: 'http://localhost:3000',
+   baseURL: 'https://difmo-crm-backend.vercel.app',
 
     headers: {
         'Content-Type': 'application/json',
