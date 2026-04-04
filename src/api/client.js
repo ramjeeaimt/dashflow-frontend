3b17ee7b56fc2549ebf986be1503c3688f71a928
difwaservices-commits <difwaservices@gmail.com>
2026-04-04 20:00:00 +0530

    Update client.js

--- src/api/client.js
+++ src/api/client.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-   // baseURL: "https://difmo-crm-backend.vercel.app",
-     baseURL: 'http://localhost:3000',
+   baseURL: "https://difmo-crm-backend.vercel.app",
+     // baseURL: 'http://localhost:3000',
     headers: {
         'Content-Type': 'application/json',
     },
